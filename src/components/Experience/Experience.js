c1885ae7122405b7a63437d69b2df59b7df5ceec
SamX191 <sourabhsuman110s@gmail.com>
2022-07-10 23:01:21 +0530

    Added Experience

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -9,6 +9,34 @@ const Experience = () =>  (
     <SectionTitle>Experience</SectionTitle>
     <SectionText>
         <SectionSubHead>
+            Frontend Intern <SectionSubHeadLink href="https://quicksell.co/" target="_blank">@QuickSell</SectionSubHeadLink>
+        </SectionSubHead>
+        <SectionSubText4>January - March,  2022</SectionSubText4>
+        QuickSell helps merchants increase their conversion rate and shorten sales cycle with a personal e-commerce strategy.
+    <List>
+        <ListItem>
+        <ListParagraph>
+            Effectively performed state management using react hooks and redux to manage the state of the app in a single place and keep
+            changes made in the app more predictable and traceable.
+        </ListParagraph>
+        </ListItem>
+
+        <ListItem>
+        <ListParagraph>
+        Wrote and deployed features in collaboration with the engineering team and worked on bug fixes and improvements.
+
+        </ListParagraph>
+        </ListItem>
+
+        <ListItem>
+        <ListParagraph>
+            Setup the Chat API for WhatsApp API trigger to run a workflow which integrates with the Firebase Admin SDK API
+        </ListParagraph>
+        </ListItem>
+    </List>
+    </SectionText>
+    <SectionText>
+        <SectionSubHead>
             Software Developer and UI/UX Intern <SectionSubHeadLink href="https://www.khetipoint.in/" target="_blank">@Khetipoint Pvt Ltd</SectionSubHeadLink>
         </SectionSubHead>
         <SectionSubText4>June - September,  2020</SectionSubText4>
